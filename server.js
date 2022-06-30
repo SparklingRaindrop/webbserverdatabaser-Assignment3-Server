d45edfbfa73cf7f913445e1e2937c5c004d31db5
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-06-30 10:15:47 +0200

    Clean

--- server.js
+++ server.js
@@ -1,52 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
-const cors = require('cors');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
 const {logHandler} = require('./js/LogHandler');
 const EventHandler = require('./js/EventHandler');
 
-const USERNAME = 'admin';
-const PASSWORD = 'aaaa';
-
 const app = express();
 const httpServer = createServer(app);
-/* const sessionMiddleware = session({
-    secret: process.env.SECRET_KEY,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24
-    },
-    resave: false
-});
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:3000',
-}));
-app.use(sessionMiddleware); */
-
-
-/* app.post('/login', (req, res) => {
-    if (!req.body.username || !req.body.password) {
-        res.status(400).send({
-            error: 'username or password is missing'
-        });
-    }
-    const {username, password} = req.body;
-    const session = req.session;
-    if (username === USERNAME && password === PASSWORD) {
-        session.authenticated = true;
-        session.save();
-        res.send({
-            message: 'Successfully logged in.'
-        })
-    } else {
-        res.send({
-            message: 'Username or password is wrong.'
-        })
-    }
-}); */
 
 const options = {
     cors: {
@@ -58,22 +18,6 @@ const options = {
 const io = new Server(httpServer, options);
 const eventHandler = new EventHandler(io);
 
-/* 
-function wrap(middleware) {
-    return function (socket, next) {
-        return middleware(socket.request, {}, next);
-    };
-}
-io.use(wrap(sessionMiddleware));
-io.use((socket, next) => {
-    const session = socket.request.session;
-    if (session && session.authenticated) {
-        next();
-    } else {
-        next(new Error("unauthorized"));
-    }
-});
-*/
 io.use((socket, next) => {
     console.log("\x1b[34m%s\x1b[0m", `ID: ${socket.id} is connected.`);
     next();
@@ -84,7 +28,7 @@ io.use(logHandler);
 io.on('connection', (socket) => {
     
     socket.on('disconnect', (reason) => {
-        if (reason === 'transport close' || reason === 'client namespace disconnect') {
+        if (socket.userName !== undefined && (reason === 'transport close' || reason === 'client namespace disconnect')) {
             eventHandler.handleDisconnect(socket.userName);
         }
     });
